8fad9d5d0dc1004416977ecb63c5e728c6e532ac
Keren Blokpoel <302893130@student.rocmondriaan.nl>
2021-03-17 16:51:15 +0100

    push

---
+++ javascript.js
@@ -0,0 +1,10 @@
+const button = document.querySelector('.button-light-on')
+console.log(button);
+button.addEventListener("click", function () {
+  const elements = document.querySelectorAll('body, h1, h2, h3, h4, h5, h6')
+  for (let i = 0; i < elements.length; i++) {
+    console.log(i);
+    console.log(elements);
+    
+  }
+});
