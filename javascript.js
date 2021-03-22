fcc62b815e8767f5118c52dc9f6a924b4a8e2bf4
Keren Blokpoel <302893130@student.rocmondriaan.nl>
2021-03-22 12:54:24 +0100

    theme switcher

--- javascript.js
+++ javascript.js
@@ -4,7 +4,7 @@ button.addEventListener("click", function () {
   const elements = document.querySelectorAll('body, h1, h2, h3, h4, h5, h6')
   for (let i = 0; i < elements.length; i++) {
     console.log(i);
-    console.log(elements);
-    
+    console.log(elements[i]);
+    elements[i].classList.toggle('button-light-on')
   }
 });
